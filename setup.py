0e2df6f5955ef1de1a554ee0a67ea6516061a914
james-alvey-42 <44205066+james-alvey-42@users.noreply.github.com>
2024-04-05 13:26:38 +0100

    Moved python versioning into requirements.txt

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    required_packages = f.read().splitlines()
+
 setup(
     name="gw_response",
     version="1.0.0",
@@ -7,17 +10,5 @@ setup(
     author="Mauro Pieroni, James Alvey",
     author_email="mauro.pieroni@cern.ch, j.b.g.alvey@uva.nl",
     packages=find_packages(),
-    install_requires=[
-        "python>=3.9;pla" "numpy>=1.26.4;python_version>='3.9'",
-        "numpy==1.24.4;python_version<'3.9'",
-        "jax>=0.4.25;python_version>='3.9'",
-        "jax==0.4.13;python_version<'3.9'",
-        "jaxlib>=0.4.25;python_version>='3.9'",
-        "jaxlib==0.4.13;python_version<'3.9'",
-        "chex>=0.1.85;python_version>='3.9'",
-        "chex==0.1.7;python_version<'3.9'",
-        "healpy>=1.16.6;python_version>='3.9'",
-        "healpy==1.16.2;python_version<'3.9'",
-        "typing_extensions==4.10.0;python_version=='3.11'",
-    ],
+    install_requires=required_packages,
 )
